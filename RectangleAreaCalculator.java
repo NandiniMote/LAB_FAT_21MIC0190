7304d5c0d2ca8145ec4bdb0b5bc156fc750ca77b
Nandini <nandinipramod.mote2021@vitstudent.ac.in>
2024-04-22 06:26:02 +0530

    added RectangleAreaCalculator program

---
+++ RectangleAreaCalculator.java
@@ -0,0 +1,15 @@
+package LAB_FAT_21MIC0190;
+
+public class RectangleAreaCalculator {
+    public static void main(String[] args) {
+        // Dimensions of the rectangle
+        double length = 5.0;
+        double width = 3.0;
+        
+        // Calculate the area
+        double area = length * width;
+        
+        // Print the result
+        System.out.println("The area of the rectangle is: " + area);
+    }
+}
